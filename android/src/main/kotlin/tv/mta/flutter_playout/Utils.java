9ae9eb48f4fa82e45066b03380568ad09c809b8f
Nguyễn Đình Dũng <48447545+dzungdinh94@users.noreply.github.com>
2021-03-24 21:30:10 +0700

    Update Utils.java

--- android/src/main/kotlin/tv/mta/flutter_playout/Utils.java
+++ android/src/main/kotlin/tv/mta/flutter_playout/Utils.java
@@ -2,17 +2,19 @@ package tv.mta.flutter_playout;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class Utils {
-
-    public static Bitmap getBitmapFromURL(String src) {
+public class Utils  extends AsyncTask<String, Void, Bitmap> {
+    public Bitmap doInBackground(String... urls) {
+        Bitmap bm = null;
         try {
-            URL url = new URL(src);
+            URL url = new URL(urls[0]);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setDoInput(true);
             connection.connect();
@@ -20,8 +22,14 @@ public class Utils {
             Bitmap myBitmap = BitmapFactory.decodeStream(input);
             return myBitmap;
         } catch (IOException e) {
-            // Log exception
-            return null;
+            Log.e("img", "Error getting bitmap", e);
         }
+        return bm;
+    }
+
+    protected void onPostExecute(Bitmap bm) {
+        //do something after execute
+
+
     }
 }
